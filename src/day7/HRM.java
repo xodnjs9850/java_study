6faba7e0331d330c345b1e99eac307cc1bf0d31f
xodnjs9850 <xodnjs9850@naver.com>
2021-03-20 23:08:37 +0900

    [basic 완성]

--- src/day7/HRM.java
+++ src/day7/HRM.java
@@ -33,7 +33,7 @@ public class HRM {
         int totalEmployee = 0;
 
         while (true) {
-            System.out.println("===========사원관리 프로그램(HRM)============");
+            System.out.println("===========사원관리 프로그램(HRM)=============");
             System.out.println("# 1. 사원 정보 신규등록");
             System.out.println("# 2. 모든 사원정보 보기");
             System.out.println("# 3. 사원정보 검색");
@@ -74,13 +74,15 @@ public class HRM {
 
                 case 2:
 
-                    System.out.println("--------전체 사원 정보 -----------");
+                    System.out.println("------------- 전체 사원 정보 -------------");
                     System.out.println("사번\t\t이름\t\t나이\t\t부서");
-                    System.out.println("--------------------------------");
+                    System.out.println("-----------------------------------------");
 
                     for (int i = 0; i < totalEmployee; i++) {
                         System.out.printf("%s\t\t%s\t\t%s\t\t%s\n", employees[i][0], employees[i][1], employees[i][2], employees[i][3]);
                     }
+
+                    System.out.println(Arrays.deepToString(employees));
 
 //                    System.out.println(Arrays.deepToString(employees));
 //                    System.out.print(employees);
@@ -88,6 +90,8 @@ public class HRM {
 //                    System.out.print(Info[2]);
 //                    System.out.print(Info[3]);
 //                    System.out.println("");
+
+
                     break;
 
                 case 3:
@@ -98,12 +102,12 @@ public class HRM {
                     boolean flag = false;
                     for (int i = 0; i < totalEmployee; i++) {
                         if (employeeInfo.equals(employees[i][0])) {
-                            System.out.println("--------조회된 사원 정보 -----------");
+                            System.out.println("------------ 조회된 사원 정보 ------------");
                             System.out.println("사번\t\t이름\t\t나이\t\t부서");
 
                             System.out.printf("%s\t\t%s\t\t%s\t\t%s\n", employees[i][0], employees[i][1], employees[i][2], employees[i][3]);
 
-                            System.out.println("--------------------------------");
+                            System.out.println("----------------------------------------");
                             flag = true;
                             break;
                         }
@@ -121,7 +125,7 @@ public class HRM {
                     for (int i = 0; i < totalEmployee; i++) {
                         if (employees[i][0].equals(personnum)) {
                             System.out.printf("%s 님의 정보를 수정합니다\n", employees[i][1]);
-                            System.out.println("1. 나이 변경 | 2. 부서변경 | 3. 취소 ");
+                            System.out.println("1. 나이 변경\t|\t2. 부서변경\t|\t3. 취소 ");
                             System.out.print("> ");
 
                             int menu02 = sc.nextInt();
@@ -150,26 +154,62 @@ public class HRM {
                 case 5:
                     System.out.println("# 삭제하실 사원의 번호를 입력하세요");
                     System.out.print("> ");
-                    String delEmpoiyee = sc.next();
+                    int delEmployee = sc.nextInt();
+                    String cast = String.valueOf(delEmployee);
+                    String[][] temp = new String[employees.length - 1][5];
 
-                    for (int i = 0; i < totalEmployee; i++) {
-                        if (employees[i][0].equals(delEmpoiyee)) {
+                    int check = -1;
+                    int count = 0;
+
+                    for (int i = 0; i < employees.length; i++) {
+                        if (cast.equals(employees[i][0])) {
                             System.out.printf("%s 님의 정보를 삭제합니다.[y/n]\n", employees[i][1]);
                             String something = sc.next();
+
                             if (something.equals("y")) {
-                                System.out.printf("\n빵!! [%s]님 사망...\n", players[turn]);
-                                for (int n = ; i < players.length - 1; i++) {
-                                    players[i] = players[i+1];
+                                int dummyData = i + 1;
+
+                                // i가 가리키는 값이 Null일 경우 (배열에 들어간 마지막 사원 다음 배열은 Null임)
+                                if (employees[dummyData].equals("null")) {
+                                    for (int x = 0; x < temp.length; x++) {
+
+                                        if (dummyData > temp.length)
+                                            break;
+                                        temp[x] = employees[dummyData - 1];
+//                                    System.out.println("");
+                                        dummyData += 1;
+                                    }
+
+                                    for (int j = 0; j < temp.length; j++) {
+                                        int sum = i + j;
+                                        if (sum >= temp.length) break;
+                                        employees[j + i] = temp[j];
+                                    }
+                                    temp = null;
+                                    totalEmployee--;
+
+                                } else {
+                                    for (int x = 0; x < temp.length; x++) {
+
+                                        if (dummyData > temp.length)
+                                            break;
+                                        temp[x] = employees[dummyData];
+                                        dummyData += 1;
+                                    }
+
+                                    for (int j = 0; j < temp.length; j++) {
+                                        int sum = i + j;
+                                        if (sum >= temp.length) break;
+                                        employees[sum] = temp[j];
+                                    }
+                                    temp = null;
+                                    totalEmployee--;
                                 }
-                                String[] temp = new String[players.length - 1];;
-                                for (int i = 0; i < temp.length; i++) {
-                                    temp[i] = players[i];
-                                }
-                                players = temp; temp = null;
-                                playerNum--;
+                                break;
                             }
                         }
                     }
+                    break;
 
                 case 6:
                     System.out.println("프로그램을 종료합니다.");
